49e05e6e9b3ae31d89f9a9c257db91858caf4999
Eliane <elianealves.melo96@gmail.com>
2022-12-14 08:23:59 -0300

    att

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+  ]
 })
 export class AppModule { }
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    //selector será o nome da tag do template
+    selector: 'navbar-component',
+    //templateUrl será todo o codigo html, a definição será em um arquivo proprio ou pode ser feito entre ''
+    templateUrl: './navbar.component.html',
+    //styleUrls será o codigo css, ou o direncionamento para o arquivo css dedicado
+    styleUrls: ['./navbar.component.scss']
+})
+
+ export class NavbarComponent {
+
+}
